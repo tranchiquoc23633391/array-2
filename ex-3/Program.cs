3e475689d6b234cacdefdf2832ca1bd2a1637d2d
Trần Chí Quốc <23633391.quoc@student.iuh.edu.vn>
2025-07-24 15:06:55 +0700

    arr-2
    bai tap 3

---
+++ ex-3/Program.cs
@@ -0,0 +1,44 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        // int[,] array = {
+        //     {1,2,3,4},
+        //     {6,7,8,9},
+        //     {3,2,1,0}
+        // };
+
+        int[,] array = new int[3, 3];
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        // tinhs  tong
+        int sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+             sum += array[i, i];
+        }
+
+        Console.Write("tong duong cheo chinh" +sum);
+
+    }
+} 
